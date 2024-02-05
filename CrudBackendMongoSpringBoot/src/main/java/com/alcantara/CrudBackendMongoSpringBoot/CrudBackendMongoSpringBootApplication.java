757364bcc4b3ff59c10a1b86d6d412f0aca8cc19
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-02-05 11:13:07 -0500

    Agregando backend SpringBoot dependencias MongoDb, SpringWeb,Lombok,Validation

---
+++ CrudBackendMongoSpringBoot/src/main/java/com/alcantara/CrudBackendMongoSpringBoot/CrudBackendMongoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.alcantara.CrudBackendMongoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBackendMongoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBackendMongoSpringBootApplication.class, args);
+	}
+
+}
---
+++ CrudBackendMongoSpringBoot/src/test/java/com/alcantara/CrudBackendMongoSpringBoot/CrudBackendMongoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcantara.CrudBackendMongoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBackendMongoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
